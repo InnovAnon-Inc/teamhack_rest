ffcfacb164f452b8a6640526bfc5983c4d3c7343
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-07 00:55:30 -0500

    update

--- dev.sh
+++ dev.sh
@@ -4,8 +4,8 @@ set -euxo nounset
 ((   $UID ))
 git pull
 git add .
-git commit -m update
-git push
+git commit -m update || :
+git push             || :
 
 python3 -m build
 python3 -m pip install --force-reinstall dist/teamhack-*.*.*-py3-none-any.whl
