7d78b7ca682d5bb8687c6f88de88e5a0479cfaad
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-06 22:28:31 -0500

    update

---
+++ dev.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -euxo nounset
+(( ! $#   ))
+((   $UID ))
+git pull
+git add .
+git commit -m update
+git push
+
+python3 -m build
+python3 -m pip install --force-reinstall dist/teamhack-*.*.*-py3-none-any.whl
+pytest
+#python3 -m teamhack_dns  & pids+=( $! )
+#python3 -m teamhack_rest & pids+=( $! )
+#wait ${pids[@]}
+
