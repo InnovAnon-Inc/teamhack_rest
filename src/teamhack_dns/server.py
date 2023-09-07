2d9da736df3a93f442b9ee1e67989624d27d8dd2
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-07 16:47:07 -0500

    update

--- src/teamhack_dns/server.py
+++ src/teamhack_dns/server.py
@@ -24,18 +24,18 @@ def handle_dns_query(conn, data):
     res = select_hostname_recordtype(conn, qname, qt)
     if not res:
       a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
-      request.add_answer(a)
-      return request.pack()
+      reply.add_answer(a)
+      return reply.pack()
     res = res[0]
     if not res:
       a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
-      request.add_answer(a)
-      return request.pack()
+      reply.add_answer(a)
+      return reply.pack()
     res = res[3]
     if not res:
       a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
-      request.add_answer(a)
-      return request.pack()
+      reply.add_answer(a)
+      return reply.pack()
     print(f'qname: {qname}, qtype: {qt}, res: {res}')
 
     #if qname in dns_records and qtype in dns_records[qname]:
