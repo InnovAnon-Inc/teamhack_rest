034a9f831c395810ac3b350fbd70e55a28315e76
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-07 11:38:46 -0500

    update

--- dev.sh
+++ dev.sh
@@ -10,7 +10,8 @@ git push             || :
 python3 -m build
 python3 -m pip install --force-reinstall dist/teamhack-*.*.*-py3-none-any.whl
 pytest
-python3 -m teamhack_db
+#python3 -m teamhack_db
+python3 -m teamhack_dns
 #python3 -m teamhack_dns  & pids+=( $! )
 #python3 -m teamhack_rest & pids+=( $! )
 #wait ${pids[@]}
